bf06b3fba8870bd173ef560f163b6ff34d9661eb
kakapo9090 <powercome93@gmail.com>
2021-11-12 16:20:38 +0900

    Initial commit

---
+++ src/test/java/com/kb/a1/KBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
